4cb30e9e635a06be105c15b7ab05e3b96636464e
apizzimenti <pizzimentianthony@gmail.com>
2015-04-05 14:26:52 -0500

    remove excess functions, create new count().js file

--- Scripts/dotPlayer.js
+++ Scripts/dotPlayer.js
@@ -5,29 +5,15 @@
 $(document).ready(function () {
     'use strict';
     
-    var pluscounter = 0, negcounter = 0;
-    
     $('#gamewindow > .play-outer').mousedown(function () {
         $('.play-outer').remove();
         var margins = setMargin();
         
         $('.newbutton, .counter').css({'display': 'block'});
-        
         countdown();
         dotChange();
+        count();
+        genData();
         removeGame();
     });
-        
-    $('button').click(function () {
-        pluscounter++;
-        dotChange();
-    });
-    
-    $('#gamewindow').mousedown(function () {
-        negcounter++;
-    });
-    
 });
-    
-    
-    
