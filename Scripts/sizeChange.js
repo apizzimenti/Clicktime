3ae1ed446fe667b46b1ea81d950447e76f93462d
Anthony Pizzimenti <pizzimentianthony@gmail.com>
2015-04-04 20:45:30 -0500

    add size preference

--- Scripts/sizeChange.js
+++ Scripts/sizeChange.js
@@ -5,21 +5,32 @@
 $(document).ready(function () {
     "use strict";
     
+    $('.play-outer').css({'display': 'none'});
+    
     $('#small').click(function () {
         $('.newbutton').css({'height': '14px', 'width': '14px'});
         $('#small').css({'border': '1px solid red'});
         $('.chooser').fadeOut(300);
+        window.setTimeout(function () {
+            $('.play-outer').fadeIn(300);
+        }, 300);
     });
     
     $('#med').click(function () {
         $('.newbutton').css({'height': '18px', 'width': '18px'});
         $('#med').css({'border': '1px solid red'});
         $('.chooser').fadeOut(300);
+        window.setTimeout(function () {
+            $('.play-outer').fadeIn(300);
+        }, 300);
     });
     
     $('#large').click(function () {
         $('.newbutton').css({'height': '22px', 'width': '22px'});
         $('#large').css({'border': '1px solid red'});
         $('.chooser').fadeOut(300);
+        window.setTimeout(function () {
+            $('.play-outer').fadeIn(300);
+        }, 300);
     });
 });
