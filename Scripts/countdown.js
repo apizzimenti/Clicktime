363e590495f782a335f9a2632587ebe7a2d19fee
Anthony Pizzimenti <pizzimentianthony@gmail.com>
2015-04-04 20:41:00 -0500

    change timing function, shorter

--- Scripts/countdown.js
+++ Scripts/countdown.js
@@ -7,13 +7,12 @@ function countdown() {
     var timer = 30, $div, $newNode, space;
     $('.counter > p').append(timer);
     
-    window.setInterval(function () {
+    var final = window.setInterval(function () {
         timer--;
         $('.counter > p').empty();
         $('.counter > p').append(timer);
+        if (timer === 0) {
+            window.clearInterval(final);
+        }
     }, 1000);
-    
-    if (timer === 0) {
-        window.clearInterval();
-    }
 }
